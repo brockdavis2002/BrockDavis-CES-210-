a131a69128201b953f191341062de8c85589448e
Brock Davis <Brockdavis246@gmail.com>
2024-04-27 21:08:18 -0600

    Finished Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Consle.write asks the user for the input in the consle if you want to 
+        //start a new line you can use Console.WriteLine
+        //string asignes the input to a designated tag
+
+        //asks user for first name and gets input
+        Console.Write("What is your first name?: ");
+        //asigns the inputed name to the tag firstname for further use
+        string firstname = Console.ReadLine();
+
+        //asks the user for the last name and gets input
+        Console.Write("What is your last name?: ");
+        //asignes the inputed last name to the tag lastname for further use
+        string lastname = Console.ReadLine();
+
+
+        //prints out the persons name formated
+        //the $ is for fomating it simmeler to using f in python
+        Console.WriteLine($"Your name is {lastname}. {firstname} {lastname}");
+
     }
 }
