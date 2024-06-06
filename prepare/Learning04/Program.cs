766618c500686e1ea26325dd79b1435b03934ae0
Brock Davis <Brockdavis246@gmail.com>
2024-06-05 21:18:07 -0600

    finished

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Create a base Assignment object
+        Assignment a1 = new Assignment("Bow wow", "Multiplication for Dogs");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Polly the parrot", "Bird Math", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Goldy the fish", "History of the Ocian", "The lockness monster");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
